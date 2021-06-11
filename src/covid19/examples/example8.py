8742a9531b4876dd3481db3eaf0aab1a6be151d3
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-11 21:40:48 +0530

    More updates to examples

--- src/covid19/examples/example8.py
+++ src/covid19/examples/example8.py
@@ -6,7 +6,7 @@ import matplotlib.ticker as tkr
 
 from covid19 import covid_data
 
-country_alpha_2 = 'IN'
+country_alpha_2 = 'US'
 country_data = covid_data.load_jhu_data()[country_alpha_2]
 country_name = country_data['country_name']
 timeseries = country_data['timeseries'][-150:]
@@ -16,36 +16,35 @@ x = list(map(
     lambda d: datetime.datetime.fromtimestamp(d['unixtime']),
     timeseries,
 ))
-y1a = list(map(
-    lambda d: d['cum_people_vaccinated'] / population,
-    timeseries,
-))
 y1 = list(map(
     lambda d: d['cum_people_fully_vaccinated'] / population,
     timeseries,
 ))
-
-days_to_second_dose1 = 7 * 10
-y2 = [0 for _ in range(0, days_to_second_dose1)] + list(map(
+y2 = list(map(
     lambda d: d['cum_people_vaccinated'] / population,
-    timeseries[:-days_to_second_dose1],
-))
-days_to_second_dose2 = 7 * 8
-y3 = [0 for _ in range(0, days_to_second_dose2)] + list(map(
-    lambda d: d['cum_people_vaccinated'] / population,
-    timeseries[:-days_to_second_dose2],
-))
-days_to_second_dose3 = 7 * 6
-y4 = [0 for _ in range(0, days_to_second_dose3)] + list(map(
-    lambda d: d['cum_people_vaccinated'] / population,
-    timeseries[:-days_to_second_dose3],
+    timeseries,
 ))
 
+legend = [
+    'Fully Vaccinated',
+    'At least 1st dose',
+]
 plt.plot(x, y1,  color="green")
-plt.plot(x, y1a,  color="lightgreen")
-plt.plot(x, y2,  color="blue")
-plt.plot(x, y3,  color="purple")
-plt.plot(x, y4,  color="red")
+plt.plot(x, y2,  color="lightgreen")
+
+windows = [4, 6, 8, 10, 12, 14]
+n_windows = len(windows)
+for i, weeks in enumerate(windows):
+    days_to_second_dose1 = 7 * weeks
+    y = [0 for _ in range(0, days_to_second_dose1)] + list(map(
+        lambda d: d['cum_people_vaccinated'] / population,
+        timeseries[:-days_to_second_dose1],
+    ))
+    r = 1
+    g = 0.8 - 0.8 * i / n_windows
+    b = g
+    plt.plot(x, y,  color=(r, g, b))
+    legend.append('%d weeks since 1st dose' % weeks)
 
 plt.title(
     '%% People Vaccinated in %s.' % (country_name)
@@ -56,13 +55,7 @@ plt.suptitle(
     fontsize=6,
 )
 plt.legend(
-    [
-        'Fully Vaccinated',
-        'At least 1st dose',
-        '%d Weeks after 1st dose' % (days_to_second_dose1 / 7),
-        '%d Weeks after 1st dose' % (days_to_second_dose2 / 7),
-        '%d Weeks after 1st dose' % (days_to_second_dose3 / 7)
-    ],
+    legend,
     loc='upper left',
 )
 
