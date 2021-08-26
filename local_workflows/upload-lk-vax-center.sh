f0ea30cbbb2603563523b24bc4e5fb122debcfaf
nuuuwan <nuuuwan@gmail.com>
2021-08-26 07:42:44 +0530

    [shift-pipelines-to-production] Updated Upload (2021-08-26-0742)

--- local_workflows/upload-lk-vax-center.sh
+++ local_workflows/upload-lk-vax-center.sh
@@ -1,3 +1,7 @@
+git checkout main
+git reset --hard HEAD
+git pull origin main
+
 git checkout data
 git pull origin data
 git clean -f -d
@@ -5,5 +9,6 @@ cp /tmp/covid19* ./
 git add .
 git commit -m "Added local_workflows/download-lk-vax-center.sh output"
 git push origin data
+
 git checkout main
 open https://github.com/nuuuwan/covid19/tree/data
